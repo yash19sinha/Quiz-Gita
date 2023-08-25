6023d24dc0832188206c02574cebd526c98b5d0d
Samridhi-Sinha <96681774+yash19sinha@users.noreply.github.com>
2023-08-25 19:35:03 +0530

    updated questions

--- src/app/_ui/content/content.ts
+++ src/app/_ui/content/content.ts
@@ -8,29 +8,29 @@ export const importantToKnow = [
 export const quizQuestions = [
   {
     question:
-      "What is the process of converting analog signals into digital data called?",
-    options: ["Encoding", "Decoding", "Encryption", "Modulation"],
-    correctAnswer: "Encoding",
+      "Name the place of pilgrimage where this battle took place.",
+    options: ["Kurukshetra", "Ayodhya", "Kashi", "Mathura"],
+    correctAnswer: "Kurukshetra",
   },
   {
-    question: "What is the smallest unit of digital information?",
-    options: ["Byte", "Bit", "Megabyte", "Gigabyte"],
-    correctAnswer: "Bit",
+    question: "How did Sanjaya have the vision of the battlefield from afar?",
+    options: ["Through a magical potion he consumed", "Because of a rare gemstone he possessed", "Sanjaya's vision of the battlefield was due to the mercy of his teacher Vyasa", "By practicing advanced meditation techniques"],
+    correctAnswer: "Sanjaya's vision of the battlefield was due to the mercy of his teacher Vyasa",
   },
   {
-    question: "Which programming language is used for web development?",
-    options: ["PHP", "Ruby", "Swift", "HTML"],
-    correctAnswer: "HTML",
+    question: "Give an example of Dronacharya's generosity",
+    options: ["He helped rebuild a village that had been destroyed in a fire", "He provided free education to all children in his village", "He offered his own chariot to a poor beggar in need", "Dronacharya taught military secrets to his rival's son, Dhristadyumna, despite knowing that he could be his future enemy"],
+    correctAnswer: "Dronacharya taught military secrets to his rival's son, Dhristadyumna, despite knowing that he could be his future enemy",
   },
   {
-    question: "What is the standard file format for images on the web?",
-    options: ["PNG", "PDF", "TXT", "DOC"],
-    correctAnswer: "PNG",
+    question: "What is an akshauhini?",
+    options: ["A form of traditional Indian medicine that emphasizes the balance of bodily humors and the use of herbal remedies to promote overall health and well-being", "An akshauhini is a military formation consisting of 21,870 chariots, elephants, cavalry, and infantry.", "An ancient method of textile dyeing that utilizes natural plant-based pigments to create vibrant and enduring colors on fabrics.", "A complex system of mathematical calculations used by ancient Indian astronomers to predict celestial events and planetary positions."],
+    correctAnswer: "An akshauhini is a military formation consisting of 21,870 chariots, elephants, cavalry, and infantry.",
   },
   {
     question:
-      "Which technology is used to track the location of a mobile device?",
-    options: ["GPS", "NFC", "Wi-Fi", "Bluetooth"],
-    correctAnswer: "GPS",
+      "Why did Duryodhana approach Dronacharya",
+    options: ["Duryodhana approached Dronacharya to inform him about the strength of the Pandavas' army and his own fear", "To consult about ways to establish better diplomatic relations with neighboring kingdoms and strengthen alliances.", "To receive teachings on ethics and morality in leadership to make well-informed decisions.", "To gain insights into the intricacies of economic management and trade policies for his kingdom."],
+    correctAnswer: "Duryodhana approached Dronacharya to inform him about the strength of the Pandavas' army and his own fear",
   },
 ];
